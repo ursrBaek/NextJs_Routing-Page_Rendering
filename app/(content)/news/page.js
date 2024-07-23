d517fefbb10fb97a175865c288e6b6dd910d845a
ursrBaek <ursrbaek@gmail.com>
2024-07-23 11:22:55 +0900

    로딩중 폴백 표시

--- app/(content)/news/page.js
+++ app/(content)/news/page.js
@@ -1,8 +1,8 @@
 import NewsList from "@/components/news-list";
 import { getAllNews } from "@/lib/news";
 
-export default function NewsPage() {
-  const news = getAllNews(); // 클라이언트측에서는 작동 x
+export default async function NewsPage() {
+  const news = await getAllNews(); // 클라이언트측에서는 작동 x
   // 클라이언트에서는 데이터베이스에 접근할 수 없기 때문
 
   return (
