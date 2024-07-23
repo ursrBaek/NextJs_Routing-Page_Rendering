0fe6919c5c08e564e251ef9a7d14b0f03dd7ce71
ursrBaek <ursrbaek@gmail.com>
2024-07-23 12:11:49 +0900

    로컬데이터소스(데이터베이스)로 마이그레이션

---
+++ components/modal-backdrop.js
@@ -0,0 +1,8 @@
+"use client";
+import { useRouter } from "next/navigation";
+import React from "react";
+
+export default function ModalBackdrop() {
+  const router = useRouter();
+  return <div className="modal-backdrop" onClick={router.back} />;
+}
